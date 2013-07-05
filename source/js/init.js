2cb268efe447c5659b007a5cdf262b05a2939a3a
Danny Martinez <dannyrubenm@gmail.com>
2013-07-05 13:10:01 -0400

    Fix buttoms

--- source/js/init.js
+++ source/js/init.js
@@ -1,10 +1,10 @@
 Modernizr.load([
-    //first test need for polyfill
-    {
-        test: window.matchMedia,
-        nope: "/javascripts/vendor/media.match.min.js"
-    },
+	//first test need for polyfill
+	{
+		test: window.matchMedia,
+		nope: "/javascripts/vendor/media.match.min.js"
+	},
 
-    //and then load enquire
-    //"/javascripts/site.js"
+	//and then load enquire
+	//"/javascripts/site.js"
 ]);
