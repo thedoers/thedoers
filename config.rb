87845b2c3aed572436e475f49d7eefa2bc5a6bfd
Piermaria Cosina <piermaria@thedoersrpoject.com>
2013-08-07 22:43:29 +0200

    faviicon

--- config.rb
+++ config.rb
@@ -54,18 +54,18 @@ set :fonts_dir, 'fonts'
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  #activate :minify_css
+  activate :minify_css
   
   # # Minify Javascript on build
   activate :minify_javascript
   
   # # Create favicon/touch icon set from source/favicon_base.png
-  #activate :favicon_maker
+  activate :favicon_maker
   
   # # Enable cache buster
   activate :cache_buster
 
-  #activate :minify_html
+  activate :minify_html
   
   # # Use relative URLs
   #activate :relative_assets
