47de7d24f69c19f202107b173f6d7b8ccb6fd70c
Piermaria Cosina <piermaria@thedoersrpoject.com>
2013-10-07 12:23:11 +0200

    cean up

--- source/js/vendor/blurscript.jsx
+++ source/js/vendor/blurscript.jsx
@@ -1,26 +1,30 @@
 ﻿#target photoshop
 var folder = Folder.  selectDialog("Enter path:");
 var fileList = folder.getFiles("*.jpg");
-fileList = folder.getFiles("*.JPG");
+var exportFolder = Folder.  selectDialog("Enter export path:");
+//fileList = folder.getFiles("*.JPG");
+//fileList = folder.getFiles("*.JPEG");
+//fileList = folder.getFiles("*.jpeg");
 
 for(i=0;i<fileList.length;i++) {
     var file = fileList[i];
     var namefile = file.name; 
-    var namefileblur = namefile.replace(".jpg","blur.jpg");
-    namefileblur = namefile.replace(".JPG","blur.JPG");
-    var namenewfile = file.path + "//" + namefileblur;    
+    var namefileblur = namefile.replace(".jpg","_low.jpg");
+    //namefileblur = namefile.replace(".JPG","blur.JPG");
+    
+    var namenewfile = exportFolder + "//" + namefileblur;    
    // alert(namenewfile);
     var docRef = app.open(file);
-   
+   /*
     docRef.activeLayer.applyGaussianBlur(10);
     docRef.saveAs(new File (namenewfile), new JPEGSaveOptions());
     app.activeDocument.close(SaveOptions.DONOTSAVECHANGES);	
-
-    /*var options = new ExportOptionsSaveForWeb();
-    options.blur = 120;
+    */
+    var options = new ExportOptionsSaveForWeb();
+    options.blur = 2;
     options.quality =0;
     options.format = SaveDocumentType.JPEG;
     docRef.exportDocument(new File (namenewfile), ExportType.SAVEFORWEB,options);
     app.activeDocument.close(SaveOptions.DONOTSAVECHANGES);     
-  */
+ 
  }
