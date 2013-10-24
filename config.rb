28acad6d70ca932ddcd7e0b14048d4059af70851
Piermaria Cosina <piermaria@thedoersproject.com>
2013-10-24 12:30:57 +0200

    gzip and image_tag

--- config.rb
+++ config.rb
@@ -69,14 +69,13 @@ configure :build do
   
   # # Minify Javascript on build
   activate :minify_javascript
-  
+  activate :minify_html
   # # Create favicon/touch icon set from source/favicon_base.png
   #activate :favicon_maker
   
   # # Enable cache buster
   activate :cache_buster
-
-  #activate :minify_html
+  #activate :image_optim
   
   # # Use relative URLs
   #activate :relative_assets
